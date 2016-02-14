63d439e2000e93bf86bbaf3a644f52b1e1f3a8de
Sudarshan Muralidhar Devardekar <sudarshansmd@gmail.com>
2016-02-14 19:31:52 +0530

    Refactoring gallery script for rendering funky gallery canvas.

--- PhotoPorto.NET4.5.2/App_Start/BundleConfig.cs
+++ PhotoPorto.NET4.5.2/App_Start/BundleConfig.cs
@@ -23,9 +23,15 @@ namespace PhotoPorto
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/gallery").Include(
+                      "~/Scripts/gallery.js"));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+
+            BundleTable.EnableOptimizations = true;
         }
     }
 }
